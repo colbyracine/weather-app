ab04668a77ac7bebae7379be8cfe387c37e6dc0a
Colby Racine <91927888+colbyracine@users.noreply.github.com>
2023-04-09 00:55:46 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+app.use(express.json());
+const cors = require("cors");
+app.use(cors());
+const bcrypt = require("bcrypt");
+
+const jwt = require("jsonwebtoken");
+
+const JWT_SECRET = "gsg16se51grg41rse6g4r85se6g1zg6r51gz56h1z65";
+
+const mongoUrl =
+  "mongodb+srv://admin:3zbU7Z8c1vYPxPsM@weatherapp.ygtopug.mongodb.net/WeatherAppUsers?retryWrites=true&w=majority";
+
+mongoose
+  .connect(mongoUrl, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((e) => console.log(e));
+
+require("./userDetails");
+
+const User = mongoose.model("UserInfo");
+app.post("/register", async (req, res) => {
+  const { fname, lname, email, password } = req.body;
+
+  const encryptedPassword = await bcrypt.hash(password, 10);
+  try {
+    const oldUser = await User.findOne({ email });
+
+    if (oldUser) {
+      return res.json({ error: "User Exists" });
+    }
+    await User.create({
+      fname,
+      lname,
+      email,
+      password: encryptedPassword,
+    });
+    res.send({ status: "ok" });
+  } catch (error) {
+    res.send({ status: "error" });
+  }
+});
+
+app.post("/login-user", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.json({ error: "User Not Found" });
+  }
+  if (await bcrypt.compare(password, user.password)) {
+    const token = jwt.sign({}, JWT_SECRET);
+
+    if (res.status(201)) {
+      return res.json({ status: "ok", data: token });
+    } else {
+      return res.json({ error: "error" });
+    }
+  }
+  res.json({ status: "error", error: "Invalid Password" });
+});
+
+app.listen(5000, () => {
+  console.log("Server Started");
+});
